e49bbe14d141d93cf5ff57bd51331f9d9bc6bd84
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-06-19 12:24:34 +0330

    front base

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::middleware([
