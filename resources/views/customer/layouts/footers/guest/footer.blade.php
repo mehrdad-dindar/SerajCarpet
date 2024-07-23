34f34658d2f5d4be8ca2a12b0321851865f8053b
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-07-23 11:01:03 +0330

    better login page footer

--- resources/views/customer/layouts/footers/guest/footer.blade.php
+++ resources/views/customer/layouts/footers/guest/footer.blade.php
@@ -2,27 +2,27 @@
   <div class="container">
     <div class="flex flex-wrap -mx-3">
       <div class="flex-shrink-0 w-full max-w-full mx-auto mb-6 text-center lg:flex-0 lg:w-8/12">
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Company </a>
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> About Us </a>
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Team </a>
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Products </a>
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Blog </a>
-        <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Pricing </a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("Company") }}</a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("About Us") }}</a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("Team") }}</a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("Services") }}</a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("Blog") }}</a>
+        <a href="#" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">{{ __("Pricing") }}</a>
       </div>
       <div class="flex-shrink-0 w-full max-w-full mx-auto mt-2 mb-6 text-center lg:flex-0 lg:w-8/12">
-        <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+        <a href="#" target="_blank" class="mr-6 text-slate-400">
           <span class="text-size-lg fab fa-dribbble"></span>
         </a>
-        <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+        <a href="#" target="_blank" class="mr-6 text-slate-400">
           <span class="text-size-lg fab fa-twitter"></span>
         </a>
-        <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+        <a href="#" target="_blank" class="mr-6 text-slate-400">
           <span class="text-size-lg fab fa-instagram"></span>
         </a>
-        <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+        <a href="#" target="_blank" class="mr-6 text-slate-400">
           <span class="text-size-lg fab fa-pinterest"></span>
         </a>
-        <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+        <a href="#" target="_blank" class="mr-6 text-slate-400">
           <span class="text-size-lg fab fa-github"></span>
         </a>
       </div>
@@ -32,10 +32,9 @@
         <p class="mb-0 text-slate-400">
           Copyright ©
           <script>
-            document.write(new Date().getFullYear());
+            document.write(new Date().getFullYear() + " - " + {{verta(now())->format("Y")}});
           </script>
-          Soft by <a class="font-semibold text-slate-700" href="https://www.creative-tim.com" target="_blank">Creative Tim</a> & <a
-            class="font-semibold text-slate-700" href="https://updivision.com" target="_blank">UPDIVISION</a>.
+          تمامی حقوق برای <a class="font-semibold text-transparent bg-gradient-cyan bg-clip-text" href="https://serajcarpet.com" target="_blank">قالیشویی سراج</a> محفوظ است.
         </p>
       </div>
     </div>
