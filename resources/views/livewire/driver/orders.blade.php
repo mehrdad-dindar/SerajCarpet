f62177a8fb106ecca564db83b67867c3f8c9c0b4
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-08-27 16:09:51 +0330

    driver orders status on table

--- resources/views/livewire/driver/orders.blade.php
+++ resources/views/livewire/driver/orders.blade.php
@@ -1,6 +1,31 @@
 <div class="relative flex flex-col w-full min-w-0 mb-0 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
-    <div class="p-6 pb-0 mb-0 bg-white rounded-t-2xl">
+    <div class="p-6 pb-0 mb-0 bg-white rounded-t-2xl flex items-center justify-between">
         <h6>{{ __("Orders List") }}</h6>
+        <x-srj-badge outline secondary :label="verta()->format('d F Y')" />
+    </div>
+    <div class="flex justify-center">
+        <div class="relative w-full md:w-3/4">
+        <ul class="relative flex flex-wrap p-1 list-none bg-transparent rounded-xl" nav-pills role="tablist">
+            <li class="z-30 flex-auto text-center">
+                <a class="z-30 block w-full px-0 py-1 mb-0 transition-all border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700" nav-link active href="javascript:;" role="tab" aria-selected="true">
+                    <x-srj-ph-icon name="truck" class="w-4 h-4"/>
+                    <span class="ml-1">{{ __("Collective Orders") }}</span>
+                </a>
+            </li>
+            <li class="z-30 flex-auto text-center">
+                <a class="z-30 block w-full px-0 py-1 mb-0 transition-all border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700" nav-link href="javascript:;" role="tab" aria-selected="false">
+                    <x-srj-ph-icon name="truck" class="w-4 h-4" variant="duotone"/>
+                    <span class="ml-1">{{ __("Delivery Orders") }}</span>
+                </a>
+            </li>
+            <li class="z-30 flex-auto text-center">
+                <a class="z-30 block w-full px-0 py-1 mb-0 transition-colors border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700" nav-link href="javascript:;" role="tab" aria-selected="false">
+                    <x-srj-ph-icon name="arrow-bend-double-up-left" class="w-4 h-4"/>
+                    <span class="ml-1">{{ __("Revisit Orders") }}</span>
+                </a>
+            </li>
+        </ul>
+    </div>
     </div>
     <div class="flex-auto px-0 pt-0 pb-2">
         <div class="p-0 overflow-x-auto">
