382d785a11eddeec4a8fbfca2fa54677f7501f9e
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-08-31 16:34:07 +0330

    order status base

---
+++ database/seeders/OrderStatusSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\OrderStatus as OrderStatusDb;
+
+class OrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            [
+                'name' => 'reserved',
+                'label' => 'رزرو شده',
+                'color' => 'primary',
+                'has_time' => false
+            ],
+            [
+                'name' => 'in_waiting_list',
+                'label' => 'در لیست جمعی قرار دارد',
+                'color' => 'warning',
+                'has_time' => false
+            ],
+            [
+                'name' => 'carpets_received',
+                'label' => 'فرش ها تحویل گرفته شده',
+                'color' => 'info',
+                'has_time' => false
+            ],
+            [
+                'name' => 'pre_wash_repair_service',
+                'label' => 'خدمات ترمیم پیش از شستشو دارد',
+                'color' => 'warning',
+                'has_time' => false
+            ],
+            [
+                'name' => 'sent_to_factory_for_washing',
+                'label' => 'جهت شستشو به کارخانه ارسال گردیده',
+                'color' => 'success',
+                'has_time' => false
+            ],
+            [
+                'name' => 'post_wash_repair_service',
+                'label' => 'خدمات ترمیم پس از شستشو دارد',
+                'color' => 'danger',
+                'has_time' => false
+            ],
+            [
+                'name' => 'ready_for_delivery_to_customer',
+                'label' => 'اماده تحویل به مشتری',
+                'color' => 'success',
+                'has_time' => false
+            ],
+            [
+                'name' => 'delivered_and_paid',
+                'label' => 'تحویل و تسویه شده',
+                'color' => 'success',
+                'has_time' => false
+            ],
+        ];
+
+        OrderStatusDb::insert($statuses);
+    }
+}
