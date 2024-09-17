d06e2214cb30ebf4bdb34ce9b29472d5fc694fc2
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-09-17 13:25:52 +0330

    get driver routes in driver dashboard

--- resources/views/livewire/driver/panel.blade.php
+++ resources/views/livewire/driver/panel.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <p class="text-center text-emerald-500 ltr">Success is as dangerous as failure.</p>
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    @foreach($routeTypes as $type)
+        @livewire("driver.order.card",['type' => $type])
+    @endforeach
 </div>
