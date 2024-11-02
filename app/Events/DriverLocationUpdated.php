a024c1f29560802cf1e1ae26c1b6f64c918c2656
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-11-02 16:03:39 +0330

    better shift managing

---
+++ app/Events/DriverLocationUpdated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DriverLocationUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $driverId;
+    public $latitude;
+    public $longitude;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($driverId, $latitude, $longitude)
+    {
+        $this->driverId = $driverId;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('driver-location.' . $this->driverId),
+        ];
+    }
+}
