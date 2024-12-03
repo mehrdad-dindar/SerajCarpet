9b628a0277ae8504b9282bd9f02037ca97fdf3ae
Mehrdad Dindar <mehrdad.replay@gmail.com>
2024-12-03 15:03:29 +0330

    preparing Comment Model for media

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Comment extends Model
+class Comment extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     protected $guarded;
 
     public function commentable()
