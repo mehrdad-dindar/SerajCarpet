1436a60f5037f29a36ecc2cc2496b9dbf73009ee
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-02-16 12:54:04 +0330

    fixed 'sharch by customer phone' and 'reserve time (shift)'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->spa()
+//            ->spa()
             ->font('iranSans')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
