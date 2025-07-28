c20899477015e016f9e949f3b3427f3ea1af150c
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-07-28 19:35:05 +0330

    better driver location

--- app/Filament/Pages/ShowDriverLocations.php
+++ app/Filament/Pages/ShowDriverLocations.php
@@ -3,12 +3,18 @@
 namespace App\Filament\Pages;
 
 use App\Models\DriverLocation;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Database\Eloquent\Model;
 
-class ShowDriverLocations extends Page
+class ShowDriverLocations extends Page implements HasTable
 {
-
+    use InteractsWithTable;
     protected static ?string $navigationIcon = 'heroicon-o-map';
     protected static ?string $activeNavigationIcon = 'heroicon-s-map';
     protected static string $view = 'filament.pages.show-driver-locations';
@@ -23,4 +29,36 @@ class ShowDriverLocations extends Page
         return "زنده";
     }
     protected static ?string $navigationBadgeTooltip = 'آخرین موقعیت راننده ها در ۳۰ دقیقه گذشته';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(DriverLocation::query())
+            ->columns([
+                TextColumn::make('driver.name')
+                    ->translateLabel(),
+                TextColumn::make('location')
+                    ->translateLabel()
+                    ->url(function (Model $record): string {
+                        return sprintf(
+                            'https://nshn.ir/?lat=%s&lng=%s',
+                            $record->latitude,
+                            $record->longitude
+                        );
+                    }, true)
+                    ->getStateUsing(fn ($record) => $record->latitude.','.$record->longitude),
+                TextColumn::make('updated_at')
+                    ->jalaliDateTime('l - d F Y')
+                    ->label('آخرین بروزرسانی'),
+            ])
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ]);
+    }
 }
